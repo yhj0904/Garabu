d57d26d49a1316e10fb561ef1a69849a3bf70a40
popori <ujk6073@gmail.com>
2024-03-23 00:37:06 +0900

    :building_construction: JWT Login...

--- src/Routes/Login.tsx
+++ src/Routes/Login.tsx
@@ -2,9 +2,8 @@ import axios from "axios";
 import { useState } from "react";
 import {Col, Row, Form, Button } from "react-bootstrap";
 
-function Register(){
+function Login(){
 
-    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
@@ -12,7 +11,6 @@ function Register(){
     const handleSaveChanges = async(e:any) =>{
       e.preventDefault();
       const res = axios.post('http://192.168.0.9:8080/join',{
-        username: name,
         email : email,
         password : password
       })
@@ -31,15 +29,6 @@ function Register(){
         </Col>
       </Form.Group>
 
-      <Form.Group as={Row} className="mb-3" controlId="formHorizontalName">
-        <Form.Label column sm={2}>
-         Name
-        </Form.Label>
-        <Col sm={10}>
-          <Form.Control type="Name" placeholder="Name" onChange={(e)=>setName(e.target.value)}/>
-        </Col>
-      </Form.Group>
-
       <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
         <Form.Label column sm={2}>
           Password
@@ -48,10 +37,16 @@ function Register(){
           <Form.Control type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
         </Col>
       </Form.Group>
+      
+      <Form.Group as={Row} className="mb-3" controlId="formHorizontalCheck">
+        <Col sm={{ span: 10, offset: 2 }}>
+          <Form.Check label="Remember me" />
+        </Col>
+      </Form.Group>
 
       <Form.Group as={Row} className="mb-3">
         <Col sm={{ span: 10, offset: 2 }}>
-          <Button type="submit">Sign in</Button>
+          <Button type="submit">Login</Button>
         </Col>
       </Form.Group>
     </Form>
@@ -61,4 +56,4 @@ function Register(){
     )
 }
 
-export default Register;
+export default Login;
